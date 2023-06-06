eaac00c4e34d8e9ed6c554edebe0a25e7319e6d4
agilesoftsystem <agilesoftsystem@gmail.com>
2023-06-06 00:04:56 -0300

    create cta button

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,10 @@ hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
 });
+
+function redirectToPage() {
+    window.open(
+        "https://api.whatsapp.com/send/?phone=19982497496&text&type=phone_number&app_absent=0",
+        "_blank",
+    );
+}
